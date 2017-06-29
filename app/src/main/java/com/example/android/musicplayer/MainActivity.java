5dcf34b0a55517982dd4acda96b7d0b22f00389e
Fey Ijaware <fshola4real@yahoo.co.uk>
2017-06-29 17:41:32 +0100

    Added in the MediaPlayer class

--- app/src/main/java/com/example/android/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -1,13 +1,59 @@
 package com.example.android.musicplayer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    //declare MediaPlayer as a global variable
+   private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.bebe);
+
+       /* ImageButton resetButton = (ImageButton) findViewById(R.id.button);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.reset();
+
+            }
+        });*/
+
+        ImageButton playButton = (ImageButton) findViewById(R.id.button2);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mediaPlayer.start();
+
+            }
+        });
+
+        ImageButton pauseButton = (ImageButton) findViewById(R.id.button3);
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.pause();
+
+            }
+        });
+
+        /*ImageButton volumeButton = (ImageButton) findViewById(R.id.button4);
+        volumeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.stop();
+
+            }
+        });*/
+
     }
 }
