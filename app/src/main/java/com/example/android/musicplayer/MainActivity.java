0097dd060ef0d8ce840fbee2da31a5c8c15823a0
Fey Ijaware <fshola4real@yahoo.co.uk>
2017-07-09 17:31:17 +0100

    Added in a callback method

--- app/src/main/java/com/example/android/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +34,13 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 mediaPlayer.start();
+                //allows you to set a callback once the music player has finished playing the media file
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        Toast.makeText(MainActivity.this, "I'm Finished", Toast.LENGTH_SHORT).show();
+                    }
+                });
 
             }
         });
